09087aa2fff89c748413169158d2ffd938a5ae87
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-03-17 02:01:51 -0700

    Update 6.Binary Tree.c

--- 6.Binary Tree.c
+++ 6.Binary Tree.c
@@ -31,7 +31,35 @@ void TR_inorder(TREE_PTR v);
 void TR_postorder(TREE_PTR v);
 
 int main() {
-    
+    int elem;
+	TREE_PTR root;
+	
+	TR_init(&root);
+	
+	/* Insertion of "10" */
+	TR_insert_root(&root, 10);
+		
+	/* Other elements insertion */
+	TR_insert_left(root, 6); 
+	TR_insert_right(root, 14);
+	
+	TR_insert_left(root->left, 5);
+	TR_insert_right(root->left, 7);
+	TR_insert_right(root->right, 19);
+	
+	TR_insert_left(root->left->left, 2);
+	TR_insert_right(root->left->right, 8);
+
+	TR_insert_right(root->left->left->left, 3);
+
+	
+	/* Tree's printing */
+	printf("\n PREORDER: ");
+	TR_preorder(root);
+	printf("\n INORDER: ");
+	TR_inorder(root);
+	printf("\n POSTORDER: ");
+	TR_postorder(root);
 }
 
 //TR_init(): initializes the tree's root 
