6b2e0e55453c4c543675aa3341cbab1db7b6f965
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-03-17 02:16:59 -0700

    Update 5.Double Linked List Reversed.c

--- 5.Double Linked List Reversed.c
+++ 5.Double Linked List Reversed.c
@@ -28,7 +28,19 @@ void DLL_print_reverse(LIST_PTR head);
 
 
 int main() {
-    
+    LIST_PTR list; 
+	
+	DLL_init(&list);
+	
+	DLL_insert_start(&list, 3);
+	DLL_insert_start(&list, 2);
+	DLL_insert_start(&list, 1);
+	
+	DLL_print(list);
+	printf("\n");
+	DLL_print_reverse(list);
+	
+	DLL_destroy(&list);
 }
 
 // DLL_init(): initializes the double linked list
