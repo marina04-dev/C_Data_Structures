2ebbe05400b838eaa78b71b5eb7fc261660b520a
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-03-17 02:15:42 -0700

    Update 5.Circular Linked List.c

--- 5.Circular Linked List.c
+++ 5.Circular Linked List.c
@@ -25,7 +25,44 @@ void CL_print(LIST_PTR head);
 void CL_destroy(LIST_PTR *head);
 
 int main() {
-    
+        LIST_PTR list; 
+	elem temp; 
+	
+	CL_init(&list);
+	
+	CL_insert_start(&list, 1);
+	CL_print(list);
+	printf("\n");
+	
+	CL_insert_start(&list, 2);
+	CL_print(list);
+	printf("\n");
+	
+	CL_insert_after(list->next, 3);
+	CL_print(list);
+	printf("\n");
+
+	CL_insert_after(list, 4);
+	CL_print(list);
+	printf("\n");
+	
+	CL_delete_start(&list, &temp);
+	CL_print(list);
+	printf("\n");
+
+	CL_delete_after(list->next, &temp);
+	CL_print(list);
+	printf("\n");
+
+	CL_delete_start(&list, &temp);
+	CL_print(list);
+	printf("\n");
+
+	CL_delete_start(&list, &temp);
+	CL_print(list);
+	printf("\n");
+	
+	CL_destroy(&list);
 }
 
 
